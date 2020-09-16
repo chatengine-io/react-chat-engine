5601869e028ade15495162e13c73d3169a0c5d3a
Adam La Morre <alamorre@gmail.com>
2020-09-15 20:11:56 -0400

    example app is now tiny

--- example/src/App.js
+++ example/src/App.js
@@ -1,17 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import { ChatEngine } from 'react-chat-engine'
 
-export default class App extends Component {
-
-  render() {
-    return (
-      <ChatEngine
-          development
-          publicKey={'1ed59673-1fd6-46ed-9eb9-56239a6a4f82'}
-          userName={'john_smith'}
-          userSecret={'pass1234'}
-      />
-    )
-  }
+export function App () {
+  return (
+    <ChatEngine
+      development
+      publicKey={'1ed59673-1fd6-46ed-9eb9-56239a6a4f82'}
+      userName={'john_smith'}
+      userSecret={'pass1234'}
+    />
+  )
 }
