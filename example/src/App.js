8944f08d62bc87e7906e0c152abbc89222f45592
Adam La Morre <alamorre@gmail.com>
2020-09-18 19:50:30 -0400

    random users in app now

--- example/src/App.js
+++ example/src/App.js
@@ -2,13 +2,27 @@ import React from 'react'
 
 import { ChatEngine } from 'react-chat-engine'
 
+const users = [
+  {
+    userName: 'john_smith',
+    userSecret: 'pass1234',
+  },
+  {
+    userName: 'adam_lamorre',
+    userSecret: 'pass1234',
+  }
+]
+
+
 export function App () {
+  const user = users[Math.floor(Math.random() * users.length)]
+
   return (
     <ChatEngine
       development
       publicKey={'1ed59673-1fd6-46ed-9eb9-56239a6a4f82'}
-      userName={'john_smith'}
-      userSecret={'pass1234'}
+      userName={user.userName}
+      userSecret={user.userSecret}
     />
   )
 }
