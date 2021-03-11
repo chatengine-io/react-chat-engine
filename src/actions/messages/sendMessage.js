da213b2800f8401bfcf9d316bb8ab87adb4f8745
Adam La Morre <alamorre@gmail.com>
2021-03-11 07:06:23 -0800

    files handled too

--- src/actions/messages/sendMessage.js
+++ src/actions/messages/sendMessage.js
@@ -3,11 +3,17 @@ import * as str from '../../actions'
 
 export function sendMessage(props, chatId, data, callback) {
     let formdata = new FormData()
-    if(data.attachments) {
+    
+    if (data.attachments) {
         for (let i = 0; i < data.attachments.length; i++) { 
             formdata.append('attachments', data.attachments[i], data.attachments[i].name)
         }
+    } else if (data.files) {
+        for (let i = 0; i < data.files.length; i++) { 
+            formdata.append('attachments', data.files[i], data.files[i].name)
+        }
     }
+
     formdata.append('text', data.text)
     formdata.append('sender_username', data.sender_username)
     formdata.append('custom_json', JSON.stringify(data.custom_json ? data.custom_json : {}))
