dde23542df4873c75d4f30ab8dea48a9b92ec535
Adam La Morre <alamorre@gmail.com>
2021-03-16 19:44:18 -0700

    lets setup easier chat feed

--- example/src/Pages/ChatWithMePage.js
+++ example/src/Pages/ChatWithMePage.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react'
 
-// import { ChatEngine } from 'react-chat-engine'
+import { Socket, ChatFeed } from 'react-chat-engine'
 
-const prod = window.location.host.indexOf('chatengine.io') !== -1
+// const prod = window.location.host.indexOf('chatengine.io') !== -1
 
 export default class HomePage extends Component {
     state = {
         loading: false,
-        rootUrl: prod ? 'https://api.chatengine.io/' : 'http://127.0.0.1:8000/',
-        projectID: prod ? '8a1f9edb-a05a-4b55-9d6e-ec399a38f5a9' : '1ed59673-1fd6-46ed-9eb9-56239a6a4f82',
-        userName: '',
-        userSecret: '',
+        c: null,
     }
     
-    render() {  
+    render() { 
         return (
             <div style={{ position: 'absolute', top: '0px', width: '100%' }}>
-                Chat wimme nah
+                <Socket 
+                    development
+                    projectID='1ed59673-1fd6-46ed-9eb9-56239a6a4f82'
+                    userName='Jane_Smith'
+                    userSecret='pass1234'
+                    onConnect={(c) => this.setState({ c, loading: false })}
+                />
+
+                {
+                    this.state.c !== null &&
+                    <ChatFeed 
+                        conn={this.state.c}    // Should it be c
+                        activeChat={296}                // These two should be grouped into Chats
+                        // chats={[{id: 296}]}
+                    />
+                }
             </div>
         )
     }
