760942326f33d5b69334f18d2b8a91b62d171fbc
Adam La Morre <alamorre@gmail.com>
2021-03-27 10:24:17 -0700

    working again

--- example/src/Pages/ChatWithMePage.js
+++ example/src/Pages/ChatWithMePage.js
@@ -13,22 +13,7 @@ export default class HomePage extends Component {
     render() { 
         return (
             <div style={{ position: 'absolute', top: '0px', width: '100%' }}>
-                <Socket 
-                    development
-                    projectID='1ed59673-1fd6-46ed-9eb9-56239a6a4f82'
-                    userName='Jane_Smith'
-                    userSecret='pass1234'
-                    onConnect={(c) => this.setState({ c, loading: false })}
-                />
-
-                {
-                    this.state.c !== null &&
-                    <ChatFeed 
-                        conn={this.state.c}    // Should it be c
-                        activeChat={296}                // These two should be grouped into Chats
-                        // chats={[{id: 296}]}
-                    />
-                }
+                hello!
             </div>
         )
     }
--- src/components/Socket/index.js
+++ src/components/Socket/index.js
@@ -1,14 +1,91 @@
-import React, { Component } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
+
+import { ChatEngineContext } from '../ChatUi/context'
+
+import { getChats } from '../../actions/chats'
+import { getMessages, readMessage } from '../../actions/messages'
 
 import { WebSocket } from 'nextjs-websocket'
-export default class Socket extends Component {
-    state = {
-        client: undefined,
-        props: null
+
+const Socket = props => {
+    const didMountRef = useRef(false)
+    const {
+      setConnecting,
+      conn, setConn,
+      chats, setChats,
+      messages, setMessages,
+      sendingMessages, setSendingMessages,
+      activeChat, setActiveChat,
+      typingCounter, setTypingCounter,
+      typingData, setTypingData,
+    } = useContext(ChatEngineContext)
+
+    function sortChats(chats) {
+        return Object.values(chats).sort((a, b) => { 
+            const aDate = a.last_message.created ? new Date(a.last_message.created) : new Date(a.created)
+            const bDate = b.last_message.created ? new Date(b.last_message.created) : new Date(b.created)
+            return new Date(bDate) - new Date(aDate); 
+        })
     }
 
-    handleEvent(event) {
-        const { props } = this
+    // Common Context Handlers
+
+    function onEditChat(chat) {
+        if (chats) {
+            const newChats = {...chats}
+            newChats[chat.id] = chat
+            setChats(newChats)
+        }
+
+        props.onEditChat && props.onEditChat(chat)
+    }
+
+    function onGetMessages(chatId, messages, connOptional) {
+        const conn = connOptional ? connOptional : props
+
+        setMessages(_.mapKeys(messages, 'id'))
+
+        if (messages.length > 0) {
+            const messageId = messages[messages.length - 1].id
+            readMessage(conn, chatId, messageId, (chat) => onEditChat(chat))
+        }
+        
+        props.onGetMessages && props.onGetMessages(chatId, messages)
+    }
+
+    function switchActiveChat(chatId, connOptional) {
+        const conn = connOptional ? connOptional : props
+
+        setActiveChat(chatId)
+
+        if (chatId) {
+            getMessages(conn, chatId, (chatId, messages) => onGetMessages(chatId, messages, conn))
+        }
+    }
+
+    function onGetChats(chats, connOptional) {
+        const conn = connOptional ? connOptional : props
+        chats = sortChats(chats)
+    
+        if (chats.length > 0 && activeChat === null) {
+            switchActiveChat(chats[0].id, conn) 
+        }
+        
+        setChats(_.mapKeys(chats, 'id'))
+    }
+
+    function onConnect(conn) {
+        setConn(conn)
+        setConnecting(false)
+    
+        getChats(conn, (chats) => onGetChats(chats, conn))
+    
+        props.onConnect && props.onConnect(conn)
+    }
+    
+    // Socket Events
+
+    function handleEvent(event) {
         const eventJSON = JSON.parse(event)
 
         if (eventJSON.action === 'login_error') {
@@ -20,60 +97,195 @@ export default class Socket extends Component {
                 Double check these credentials to make sure they're correct.\n
                 If all three are correct, try resetting the username and secret in the Online Dashboard or Private API.`
             )
-            this.props.onFailAuth && this.props.onFailAuth(this.props)
 
-        } else if (eventJSON.action === 'is_typing') {
-            props.onTyping && props.onTyping(eventJSON.data.id, eventJSON.data.person)
+            setConn(undefined)
+
+            props.onFailAuth && props.onFailAuth(conn)
 
         } else if (eventJSON.action === 'new_chat') {
+            const chat = eventJSON.data
+            
+            if (chats) {
+                const newChats = {...chats}
+                newChats[chat.id] = chat
+                setChats(newChats)
+                switchActiveChat(chat.id)
+            }
+
             props.onNewChat && props.onNewChat(eventJSON.data)
 
         } else if (eventJSON.action === 'edit_chat') {
+            onEditChat(eventJSON.data)
+            
             props.onEditChat && props.onEditChat(eventJSON.data)
 
         } else if (eventJSON.action === 'delete_chat') {
-            props.onDeleteChat && props.onDeleteChat(eventJSON.data)
+            const chat = eventJSON.data 
+
+            if (chats) {
+                chats[chat.id] = undefined
+                
+                setChats(chats)
+          
+                if (!_.isEmpty(chats)) {
+                    const sortedChats = sortChats(chats)
+                    switchActiveChat(sortedChats[0] ? parseInt(sortedChats[0].id) : 0)
+                }
+            }
+
+            props.onDeleteChat && props.onDeleteChat(chat)
 
         } else if (eventJSON.action === 'add_person') {
+            onEditChat(eventJSON.data)
+            
             props.onAddPerson && props.onAddPerson(eventJSON.data)
 
         } else if (eventJSON.action === 'remove_person') {
+            onEditChat(eventJSON.data)
+
             props.onRemovePerson && props.onRemovePerson(eventJSON.data)
 
         } else if (eventJSON.action === 'new_message') {
-            props.onNewMessage && props.onNewMessage(eventJSON.data.id, eventJSON.data.message)
+            const { id, message } = eventJSON.data
+
+            if (typeof message.custom_json === "string" && message.custom_json.indexOf('sender_id') !== -1) {
+                sendingMessages[JSON.parse(message.custom_json).sender_id] = undefined
+                setSendingMessages(sendingMessages)
+            }
+        
+            if (id === activeChat) {
+                const newMessages = {...messages}
+                newMessages[message.id] = message
+                setMessages(newMessages)
+            }
+          
+            readMessage(conn, activeChat, message.id, (chat) => onEditChat(chat))
+
+            props.onNewMessage && props.onNewMessage(id, message)
 
         } else if (eventJSON.action === 'edit_message') {
-            props.onEditMessage && props.onEditMessage(eventJSON.data.id, eventJSON.data.message)
+            const { id, message } = eventJSON.data
+            
+            if (id === activeChat) {
+                messages[message.id] = message
+                setMessages(messages)
+            }
+
+            props.onEditMessage && props.onEditMessage(id, message)
 
         } else if (eventJSON.action === 'delete_message') {
-            props.onDeleteMessage && props.onDeleteMessage(eventJSON.data.id, eventJSON.data.message)
+            const { id, message } = eventJSON.data
 
+            if (id === activeChat) {
+                messages[message.id] = undefined
+                setMessages(messages)
+            }
+
+            props.onDeleteMessage && props.onDeleteMessage(id, message)
+        
+        } else if (eventJSON.action === 'is_typing') {
+            const { id, person } = eventJSON.data
+            let newTypingCounter = {...typingCounter}
+
+            if (newTypingCounter[id] && newTypingCounter[id][person]) {
+                newTypingCounter = {
+                    ...newTypingCounter,
+                    [id]: {
+                    ...newTypingCounter[id],
+                    [person]: newTypingCounter[id][person] + 1
+                    }
+                }
+
+            } else {
+                newTypingCounter = {
+                    ...newTypingCounter,
+                    [id]: {
+                    ...newTypingCounter[id],
+                    [person]: 1
+                    }
+                }
+            }
+                
+            setTypingCounter(newTypingCounter)
+                
+            props.onTyping && props.onTyping(id, person)
         }
     }
 
-    onClose() { this.props.onDisconnect && this.props.onDisconnect() }
+    function onClose() { setConnecting(true) }
 
-    render() {
-        const { 
-            publicKey, projectID, 
-            userName, 
-            userPassword, userSecret, 
-            development 
-        } = this.props 
-        
-        const wsStart = development ? 'ws://' : 'wss://'
-        const rootHost = development ? '127.0.0.1:8000' : 'api.chatengine.io'
-        
-        const project = publicKey ? publicKey : projectID
-        const secret = userPassword ? userPassword : userSecret
+    // Component Lifecycle
 
-        return <WebSocket 
-            url={`${wsStart}${rootHost}/person/?publicKey=${project}&username=${userName}&secret=${secret}`}
-            onOpen={() => this.props.onConnect && this.props.onConnect(this.props)}
-            onClose={this.onClose.bind(this)}
-            onMessage={this.handleEvent.bind(this)}
-            reconnectIntervalInMilliSeconds={3000}
-        />
-    }
+    useEffect(() => {
+        if (!didMountRef.current) {
+          didMountRef.current = true
+    
+        } else {
+            Object.keys(typingCounter).map((chat) => {
+                let typers = []
+        
+                Object.keys(typingCounter[chat]).map((person) => {
+                    if (typingCounter[chat][person] > 0) {
+                        typers.push(person)
+                    }
+                })
+        
+                if (!typingData[chat] || typingData[chat].length !== typers.length) {
+                    setTypingData({ ...typingData, [chat]: typers })
+                }
+            })
+        }
+    })
+
+    useEffect(() => {
+        if (activeChat) {
+            getMessages(conn, activeChat, (chatId, messages) => onGetMessages(chatId, messages, conn))
+        }
+    }, [activeChat])
+
+    useEffect(() => { // TODO: Is typing is super shitty
+      if (typingCounter) {
+        const newTypingCounter = {...typingCounter}
+        Object.keys(newTypingCounter).map(chatId => {
+          Object.keys(newTypingCounter[chatId]).map(person => {
+            if (newTypingCounter[chatId][person] > 0) {
+              setTimeout(() => {
+                setTypingCounter({
+                  ...newTypingCounter,
+                  [chatId]: {
+                    ...newTypingCounter[chatId],
+                    [person]: newTypingCounter[chatId][person] - 1
+                  }
+                })
+              }, 2500)
+            }
+          })
+        })
+      }
+    }, [typingCounter])
+
+    // Render
+    
+    const { 
+        publicKey, projectID, 
+        userName, 
+        userPassword, userSecret, 
+        development 
+    } = props 
+    
+    const wsStart = development ? 'ws://' : 'wss://'
+    const rootHost = development ? '127.0.0.1:8000' : 'api.chatengine.io'
+    
+    const project = publicKey ? publicKey : projectID
+    const secret = userPassword ? userPassword : userSecret
+
+    return <WebSocket 
+        url={`${wsStart}${rootHost}/person/?publicKey=${project}&username=${userName}&secret=${secret}`}
+        onOpen={() => onConnect(props)}
+        onClose={onClose.bind(this)}
+        onMessage={handleEvent.bind(this)}
+        reconnectIntervalInMilliSeconds={3000}
+    />
 }
+
+export default Socket
