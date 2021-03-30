780d3608e3d91e0b3bcd6a596409afaa51098cd6
Adam La Morre <alamorre@gmail.com>
2021-03-30 06:07:31 -0700

    revision on DCs

--- example/src/Pages/DirectChatsPage.js
+++ example/src/Pages/DirectChatsPage.js
@@ -2,23 +2,16 @@ import React, { Component } from 'react'
 
 import { ChatEngine, newChat, addPerson } from 'react-chat-engine'
 
-const props = {
-    // development: true,
-    userName: 'Adam_La_Morre',
-    userSecret: 'pass1234',
-    projectID: '1ed59673-1fd6-46ed-9eb9-56239a6a4f82',
-}
-
 export default class DirectChatPage extends Component {    
     state = {
         username: ''
     }
 
-    createDirectChat() {
+    createDirectChat(creds) {
         newChat(
-            props,
+            creds,
             {is_direct_chat: true},
-            (chat) => addPerson(props, chat.id, this.state.username)
+            (chat) => addPerson(creds, chat.id, this.state.username)
         )
     }
 
@@ -26,7 +19,7 @@ export default class DirectChatPage extends Component {
         return (
             <div>
                 <input placeholder='Username' value={this.state.username} onChange={(e) => this.setState({username: e.target.value})} />
-                <button onClick={() => this.createDirectChat()}>Chat</button>
+                <button onClick={() => this.createDirectChat(creds)}>Chat</button>
             </div>
         )
     }
@@ -36,7 +29,9 @@ export default class DirectChatPage extends Component {
             <div style={{ position: 'absolute', top: '0px', width: '100%' }}>
                 <ChatEngine 
                     height='100vh'
-                    {...props}
+                    userName={'Adam_La_Morre'}
+                    userSecret={'pass1234'}
+                    projectID={'1ed59673-1fd6-46ed-9eb9-56239a6a4f82'}
                     renderNewChatForm={(creds) => this.renderChatForm(creds)}
                 />
             </div>
