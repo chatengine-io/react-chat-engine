5df790ae09d4524caed48b88fa26ab4afba53ff7
Adam La Morre <alamorre@gmail.com>
2021-04-20 07:50:19 -0400

    patch UserSocketPage

--- example/src/Pages/UserSocketFeedPage/index.js
+++ example/src/Pages/UserSocketFeedPage/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import { ChatEngineWrapper, Socket, ChatFeed } from 'react-chat-engine'
 
@@ -7,33 +7,28 @@ import { setConfiguration } from 'react-grid-system';
  
 setConfiguration({ maxScreenClass: 'xl', gutterWidth: 0 });
 
-const prod = false // window.location.host.indexOf('chatengine.io') !== -1
+const prod = window.location.host.indexOf('chatengine.io') !== -1
 
 const projectID = prod ? '...' : '1ed59673-1fd6-46ed-9eb9-56239a6a4f82'
 const chatID = prod ? 0 : 251
 
-export default class HomePage extends Component {
-    state = {
-        loading: false,
-        c: null,
-    }
-    
-    render() { 
-        return (
-            <Row>
-                <Col xs={12} sm={6} md={4} style={{ height: '600px' }}>
-                    <ChatEngineWrapper>
-                        <Socket 
-                            development={!prod}
-                            projectID={projectID}
-                            userName={'Adam_La_Morre'}
-                            userSecret={'pass1234'}
-                        />
+const UserSocketPage = () => {
+    return (
+        <Row>
+            <Col xs={12} sm={6} md={4} style={{ height: '600px' }}>
+                <ChatEngineWrapper>
+                    <Socket 
+                        development={!prod}
+                        projectID={projectID}
+                        userName={'Adam_La_Morre'}
+                        userSecret={'pass1234'}
+                    />
 
-                        <ChatFeed development={!prod} activeChat={chatID} /> 
-                    </ChatEngineWrapper>
-                </Col>
-            </Row>
-        )
-    }
+                    <ChatFeed development={!prod} activeChat={chatID} /> 
+                </ChatEngineWrapper>
+            </Col>
+        </Row>
+    )
 }
+
+export default UserSocketPage
