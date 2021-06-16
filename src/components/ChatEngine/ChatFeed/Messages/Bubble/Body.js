ca969cf156448f3ff7319c4dbfa0ac4b4887bd4a
Adam La Morre <alamorre@gmail.com>
2021-06-16 11:13:39 -0700

    bug for null msgs

--- src/components/ChatEngine/ChatFeed/Messages/Bubble/Body.js
+++ src/components/ChatEngine/ChatFeed/Messages/Bubble/Body.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Body = props => {
-    let { text } = props 
+    let text = props.text ? props.text : ''
     text = text.replaceAll("<p>", "<div>").replaceAll("</p>", "</div>")
     text = text.replaceAll("<a ", `<a style="color: ${ props.myMessage ? 'white' : '#1890ff' };" `)
 
